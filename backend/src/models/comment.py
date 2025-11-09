198a4a0a2955b5bc2deb2283a4cf41c76f5ddfe3
Dmitry <Lostghost23@yandex.ru>
2025-11-09 10:23:46 +0300

    fix: import

--- backend/src/models/comment.py
+++ backend/src/models/comment.py
@@ -1,10 +1,11 @@
 import sqlalchemy as sa
+from sqlalchemy.orm import relationship
 from ..database import Base
 
 
 class Comment(Base):
     __tablename__ = "comments"
-
+    
     id = sa.Column(sa.Integer, primary_key=True, index=True)
     body = sa.Column(sa.String, nullable=False)
     article_id = sa.Column(sa.Integer, sa.ForeignKey("articles.id"))
