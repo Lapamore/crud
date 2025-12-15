61df3435e48ab000a594a32bd67e31568cdc0e76
Dmitry <Lostghost23@yandex.ru>
2025-12-15 19:11:29 +0300

    фцука

--- users_service/src/modules/users/repositories/impl/SqlAlchemyUserWriteRepository.py
+++ users_service/src/modules/users/repositories/impl/SqlAlchemyUserWriteRepository.py
@@ -5,7 +5,7 @@ from sqlalchemy.future import select
 
 from ..core import IUserWriteRepository
 from ...dto import UserDTO
-from models.User import User
+from models import User
 
 
 class SqlAlchemyUserWriteRepository(IUserWriteRepository):
